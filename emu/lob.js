24670a05d3a510efea570c8f291162357f47d7d0
SuperSonic <supersonic@livemail.tw>
2020-06-10 23:23:50 +0800

    Create lob.js and add Alpine and Arch

---
+++ emu/lob.js
@@ -0,0 +1,13 @@
+// (c) 2020 SuperSonic (https://github.com/supersonictw)
+
+// Auto Redirect
+switch (function () {
+    var reg = new RegExp("(^|&)profile=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}()) {
+    case null:
+        window.location.href = "?profile=default";
+        break;
+}
