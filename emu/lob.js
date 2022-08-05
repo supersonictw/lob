212cf6fee67c365aec693a84582cfc83df6383b9
SuperSonic <supersonic@livemail.tw>
2022-08-05 07:28:26 +0000

    Update

--- emu/lob.js
+++ emu/lob.js
@@ -1,20 +1,30 @@
 // (c) 2020 SuperSonic (https://github.com/supersonictw)
 
 // Auto Redirect
-switch (function () {
-    var reg = new RegExp("(^|&)profile=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]);
-    return null;
-}()) {
-    case "default":
-    case "linux":
-        break;
+(function () {
+    const search = location.search;
+    const params = new URLSearchParams(search);
+    switch (params.get("profile")) {
+        case "default":
+        case "linux":
+            break;
+        case null:
+            window.location.href = "?profile=default";
+            break;
+        default:
+            alert("Unknown Operating System");
+    }
+})();
 
-    case null:
-        window.location.href = "?profile=default";
-        break;
-
-    default:
-        alert("Unknown Operating System");
-}
+// State
+const lobState = (function () {
+    const store = function () {
+        this.memory = {};
+    };
+    store.prototype.get = (name) => {
+        return this.memory[name];
+    };
+    store.prototype.set = (name, value) => {
+        this.memory[name] = value;
+    };
+})();
