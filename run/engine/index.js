2e17b107cd495917353eb0dfea5aaa2e402b1a63
supersonictw <supersonic@livemail.tw>
2022-08-06 00:13:12 +0000

    Deploying to gh-pages from @ supersonictw/lob@a5789c99335b89d54454108c0675030bc65ccea2 🚀

--- run/engine/index.js
+++ run/engine/index.js
@@ -12,7 +12,7 @@ var app = new Vue({
                 vga_memory_size: 8 * 1024 * 1024,
                 cdrom: {
                     url: "./images/system.iso",
-                    size: 37748736,
+                    size: 23068672,
                 }
             },
             vanilla: {
